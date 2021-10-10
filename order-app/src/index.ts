f49bcb23090e11cbd2201ca4a5194bad09953bf6
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2021-10-11 04:42:46 +0800

    Use ts-node to run typescript code directly

--- order-app/src/index.ts
+++ order-app/src/index.ts
@@ -3,7 +3,7 @@ import fastify from 'fastify'
 const server = fastify()
 
 server.get('/ping', async (request, reply) => {
-  return 'pong\n'
+  return 'pong pong pong\n'
 })
 
 server.listen(4000, (err, address) => {
