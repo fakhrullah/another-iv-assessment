e2b9f2b9d4cce549e0d0fee9f025b6fd9e6ca293
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2021-10-14 00:22:12 +0800

    DONE implement payment service

--- payment-app/src/index.ts
+++ payment-app/src/index.ts
@@ -1,12 +1,47 @@
-import fastify from 'fastify';
+import axios from 'axios';
+import fastify, { FastifyRequest } from 'fastify';
 import config from './config';
 
 const server = fastify()
 
-server.get('/', {}, async (req, reply) => {
+type PaymentRequest = FastifyRequest<{
+  Body: {
+    order_id: string
+    user_id: string
+  }
+}>
 
-  reply.send('success');
-});
+server.post(
+  '/', 
+  {},
+  async (req: PaymentRequest, reply) => {
+    const isOrderConfirmed: boolean = Math.floor(Math.random() * 100) % 2 === 0;
+    const orderStatus: string = isOrderConfirmed ? 'confirmed' : 'cancelled';
+
+    const {order_id, user_id} = req.body;
+
+    axios({
+      method: 'PUT',
+      url: `${config.ORDER_APP_URL}/orders/${order_id}/status`,
+      headers: {
+        user_id,
+      },
+      data: {
+        status: orderStatus,
+      }
+    })
+      .then((response) => {
+        // console.log(response);
+        console.log('SHOULD Record data to database')
+      })
+      .catch(err => { 
+        // console.log(err);
+        console.log('Error update Order Status')
+      });
+
+    reply.send(`Payment ${orderStatus}`);
+  }
+);
 
 server.listen(config.PORT, (err, address) => {
   if (err) {
