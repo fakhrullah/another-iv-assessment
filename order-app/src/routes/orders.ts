3fb396b29467f40c17c91021aab5a52af2c263a5
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2021-10-13 17:31:08 +0800

    Handle GET /orders
    Return orders.
    Models is not confirmed yet.
    OrderModel, OrderDetailModel

--- order-app/src/routes/orders.ts
+++ order-app/src/routes/orders.ts
@@ -3,7 +3,7 @@ import {
 } from 'fastify';
 import { Knex } from 'knex';
 
-interface FastifyInstanceWithKnex extends FastifyInstance {
+export interface FastifyInstanceWithKnex extends FastifyInstance {
   knex: Knex
 }
 
@@ -11,8 +11,13 @@ module.exports = (fastify: FastifyInstanceWithKnex, opts: FastifyPluginOptions, 
   // Fetch all
   const getAllOrderOpts = {
     schema: {
+      // Header required user_id
       headers: {
-        user_id: { type: 'string' },
+        type: 'object',
+        required: ['user_id'],
+        properties: {
+          user_id: { type: 'string' },
+        },
       },
     },
   };
@@ -24,20 +29,38 @@ module.exports = (fastify: FastifyInstanceWithKnex, opts: FastifyPluginOptions, 
       // eslint-disable-next-line @typescript-eslint/naming-convention
       const { user_id } = req.headers;
       console.log(user_id);
-      fastify.knex.schema.createTable('cats', (table) => {
-        table.uuid('id');
-        table.string('isPersian');
-      })
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((err) => {
-          console.log('--------errror-----------');
-          console.log(err);
-        });
-      // console.log(catsTable);
-      // fastify.knex('cats').insert({ isPersian: 'yes lah' });
-      reply.send('hello');
+
+      try {
+        const orders = await fastify.knex
+          .select('*')
+          .from('orders')
+          .join('order_status', { 'orders.status': 'order_status.key' })
+          .where('user_id', user_id);
+
+        const orderIds: string[] = orders.map<string>((order) => order.id);
+
+        const orderDetails = await fastify.knex
+          .select('*')
+          .from('order_detail')
+          // .leftJoin('order_detail', { 'orders.id': 'order_detail.order_id' })
+          .whereIn('order_id', orderIds);
+
+        const groupedOrderDetails = orderDetails.reduce((acc, curr) => {
+          if (!acc[curr.order_id]) acc[curr.order_id] = []; // If this type wasn't previously stored
+          acc[curr.order_id].push(curr);
+          return acc;
+        }, {});
+
+        const mappedOrders = orders.map((order) => ({
+          ...order,
+          order_detail: groupedOrderDetails[order.id],
+        }));
+
+        // reply.send(orders);
+        reply.send({ orders: mappedOrders });
+      } catch (error) {
+        reply.send(error);
+      }
     },
   );
 
